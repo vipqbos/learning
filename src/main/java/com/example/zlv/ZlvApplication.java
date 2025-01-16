7e682b84863de9ead3029c8983ee925e397a87ef
qbos <qbos@outlook.com>
2025-01-16 19:30:59 +0800

    first

---
+++ src/main/java/com/example/zlv/ZlvApplication.java
@@ -0,0 +1,13 @@
+package com.example.zlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zlv/ZlvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zlv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
