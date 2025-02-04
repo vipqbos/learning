07855121bfeaf0b044b82d96d8e7804c5f37428a
qbos <qbos@outlook.com>
2025-02-04 18:42:29 +0800

    前端本地化依赖的js，拓展图片传输

--- src/main/java/com/example/zlv/vo/Message.java
+++ src/main/java/com/example/zlv/vo/Message.java
@@ -2,8 +2,13 @@ package com.example.zlv.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Message {
     private String role;
+    // 文本内容
     private String content;
+    // 图片base64的图片
+    private List<String> images;
 }
