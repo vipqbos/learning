59cee90ed82dc27b3921010623a2ef39b9e77503
qbos <qbos@outlook.com>
2025-04-10 20:21:30 +0800

    对接advisor 日志拦截

--- src/main/java/com/example/zlv/config/OllamaConfig.java
+++ src/main/java/com/example/zlv/config/OllamaConfig.java
@@ -1,6 +1,7 @@
 package com.example.zlv.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class OllamaConfig {
     @Bean
     public ChatClient chatClient(OllamaChatModel model) {
-        return ChatClient.builder(model).defaultSystem("你是可爱得助手，名字叫小团团").build();
+        return ChatClient.builder(model)
+                .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请一小团团的身份和语气回答问题。")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
     }
 }
